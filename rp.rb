57c4768890c95c90a09bd9f8d8be43ce5431dce2
brenodamata <bjrdamata@gmail.com>
2019-02-27 11:11:05 -0500

    First release

---
+++ rp.rb
@@ -0,0 +1,23 @@
+ONE_REP_MAX = 60
+
+WEEK_ONE = {
+  D1: {percentage: 30, interval: 60},
+  D2: {percentage: 50, interval: 60},
+  D3: {percentage: 60, interval: 45},
+  D4: {percentage: 25, interval: 60},
+  D5: {percentage: 45, interval: 30},
+  D6: {percentage: 40, interval: 60},
+  D7: {percentage: 20, interval: 90}
+}
+
+WEEK_TWO = {
+  D1: {percentage: 35, interval: 45},
+  D2: {percentage: 55, interval: 20},
+  D3: {percentage: 30, interval: 15},
+  D4: {percentage: 65, interval: 60},
+  D5: {percentage: 35, interval: 45},
+  D6: {percentage: 45, interval: 60},
+  D7: {percentage: 25, interval: 120}
+}
+
+
